865d2cc466acd16118a9e50b7736dee79ef66fae
unknown <dpwoans@gmail.com>
2023-06-23 09:20:04 +0900

    Japanese's korean means translate to english, and another language by english

---
+++ papago_translator.py
@@ -0,0 +1,68 @@
+import os
+import sys
+import urllib.request
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+
+excel_file = pd.read_excel("c:/Users/Jaemoon/Synology_Jaemnani/SynologyDrive/study/software/naver_jlpt_words.xlsx")
+hiragana = excel_file[0]
+hanmoon = excel_file[1]
+level = excel_file[2]
+classes = excel_file[3]
+means = excel_file[4]
+means_np = means.to_numpy()[1:]
+
+set_hiragana = hiragana[1:].to_numpy()
+set_mean = means[1:].to_numpy()
+
+client_id = "xxxx" # 개발자센터에서 발급받은 Client ID 값
+client_secret = "xxxx" # 개발자센터에서 발급받은 Client Secret 값
+
+total_targets = np.array(["ja", "ko", "en","zh-CN","zh-TW","es","fr","de","ru","pt","it","vi","th","id","hi"])
+targets = ["zh-CN","zh-TW","es","fr","de","ru","pt","it","vi","th","id","hi"]
+
+# 한국어 = ko # 영어 = en # 일본어 = ja # 중국어(간체) = zh-CN # 중국어(번체) = zh_TW # 스페인어 = es
+# 프랑스어 = fr # 독일어 = de # 러시아어 = ru # 포르투갈어 = pt # 이탈리아어 = it # 베트남어 = vi
+# 태국어 = th # 인도네시아어 = id # 힌디어 = hi
+url = "https://openapi.naver.com/v1/papago/n2mt"
+request = urllib.request.Request(url)
+request.add_header("X-Naver-Client-Id",client_id)
+request.add_header("X-Naver-Client-Secret",client_secret)
+
+for i, m in enumerate(tqdm(means_np)):
+    encText = urllib.parse.quote( m )
+    print("target is ", m)
+    data = "source=ko&target=en&text=" + encText
+    response = urllib.request.urlopen(request, data=data.encode("utf-8"))
+    rescode = response.getcode()
+    result = []
+    result.append(set_hiragana[i])
+    result.append(set_mean[i])
+
+    if(rescode == 200):
+        response_body = response.read()
+        # print(response_body.decode('utf-8'))
+        en_result = response_body.decode("utf-8").split("translatedText\":\"")[1].split("\",\"")[0]
+        result.append(en_result)
+        encText = urllib.parse.quote(en_result)
+        for t in targets:
+            data = "source=en&target="+t+"&text=" + encText
+            response = urllib.request.urlopen(request, data=data.encode("utf-8"))
+            rescode = response.getcode()
+        
+            if(rescode==200):
+                response_body = response.read()
+                sub_res = response_body.decode("utf-8").split("translatedText\":\"")[1].split("\",\"")[0]
+                result.append(sub_res)            
+            else:
+                result.append("")
+    else:
+        print("pass")
+        result = ["","","","","","","","","","","","","","",""]
+    total_targets = np.vstack((total_targets, np.array(result)))    
+    print(result)
+
+df = pd.DataFrame(total_targets)
+df.to_excel("./allclass_translator.xlsx", index=False)
+print("done")
